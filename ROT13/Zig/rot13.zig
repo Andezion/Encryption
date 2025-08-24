4e3845477c5a166a0bd398585cf4b3b7656ce51e
Andezion <115638748+Andezion@users.noreply.github.com>
2025-08-24 23:12:43 +0200

    Update rot13.zig

--- ROT13/Zig/rot13.zig
+++ ROT13/Zig/rot13.zig
@@ -7,45 +7,31 @@ pub const ROT13 = struct {
         return ROT13{ .input = input };
     }
 
-    pub fn encode(self: *const ROT13, allocator: std.mem.Allocator) ![]u8 {
+    fn transform(input: []const u8, allocator: std.mem.Allocator) ![]u8 {
         var output = std.ArrayList(u8).init(allocator);
         defer output.deinit();
 
-        for (self.input) |c| {
+        for (input) |c| {
             if (c >= 'a' and c <= 'z') {
                 const encoded = ((c - 'a' + 13) % 26) + 'a';
                 try output.append(encoded);
             } else if (c >= 'A' and c <= 'Z') {
                 const encoded = ((c - 'A' + 13) % 26) + 'A';
                 try output.append(encoded);
-            } else if (std.ascii.isWhitespace(c) or std.ascii.isPunct(c)) {
-                try output.append(c);
             } else {
-                return error.UnknownSymbol;
+                try output.append(c);
             }
         }
 
         return output.toOwnedSlice();
     }
 
+    pub fn encode(self: *const ROT13, allocator: std.mem.Allocator) ![]u8 {
+        return try ROT13.transform(self.input, allocator);
+    }
+
     pub fn decode(self: *const ROT13, encoded_version: []const u8, allocator: std.mem.Allocator) ![]u8 {
-        var output = std.ArrayList(u8).init(allocator);
-        defer output.deinit();
-
-        for (encoded_version) |c| {
-            if (c >= 'a' and c <= 'z') {
-                const decoded = ((c - 'a' + 13) % 26) + 'a';
-                try output.append(decoded);
-            } else if (c >= 'A' and c <= 'Z') {
-                const decoded = ((c - 'A' + 13) % 26) + 'A';
-                try output.append(decoded);
-            } else if (std.ascii.isWhitespace(c) or std.ascii.isPunct(c)) {
-                try output.append(c);
-            } else {
-                return error.UnknownSymbol;
-            }
-        }
-
-        return output.toOwnedSlice();
+        _ = self; 
+        return try ROT13.transform(encoded_version, allocator);
     }
 };
